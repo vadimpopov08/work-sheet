9d8fa2e1ee7ef3ec08b70ac038c73bdb7842b625
vadimpopov08 <122273142+vadimpopov08@users.noreply.github.com>
2024-01-22 16:52:04 +0500

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,8 +1,25 @@
 const myButton = document.getElementById('myButton')
 const nameInput = document.getElementById('nameInput')
 const list = document.getElementById('list')
+myButton.addEventListener('click', (eve) => {
+    const newItem = document.createElement('li');
+    newItem.classList.add('item');
 
-myButton.addEventListener('click', function (eve) {
-    eve.preventDefault();
-    console.log('Тест')
-})
+
+    newItem.textContent = nameInput.value;
+
+    // Добавить кнопку для удаления элемента списка
+    const deleteButton = document.createElement('button');
+    deleteButton.textContent = 'Удалить';
+
+    // Удаление элемента списка
+    deleteButton.addEventListener('click', () => {
+        list.removeChild(newItem)
+    })
+
+    newItem.appendChild(deleteButton);
+    list.appendChild(newItem);
+
+    nameInput.value = '';
+}
+)
